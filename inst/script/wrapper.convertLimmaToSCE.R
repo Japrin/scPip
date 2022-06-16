bb244f467ccd99f7112c61aafc3905c8d0f0474f
Japrin <tail2013@gmail.com>
2022-06-16 21:12:32 +0800

    add --minCells for wrapper.convertLimmaToSCE.R

--- inst/script/wrapper.convertLimmaToSCE.R
+++ inst/script/wrapper.convertLimmaToSCE.R
@@ -6,6 +6,7 @@ parser <- ArgumentParser()
 parser$add_argument("-i", "--limmaFile", type="character", required=TRUE, help="input limma file list")
 parser$add_argument("-o", "--outPrefix", type="character", required=TRUE, help="outPrefix")
 parser$add_argument("-c", "--occ", type="double", default=0.85, help="genes detected in >= OCC datasets will be kept [default %(default)s]")
+parser$add_argument("-m", "--minCells", type="integer",default=30L, help="minimum number of cells in the clusters [default %(default)s]")
 parser$add_argument("-n", "--ncores", type="integer",default=16L, help="[default %(default)s]")
 #parser$add_argument("-m", "--measurement",type="character",default="counts",help="[default %(default)s]")
 args <- parser$parse_args()
@@ -16,6 +17,7 @@ in.file <- args$limmaFile
 out.prefix <- args$outPrefix
 opt.gene.occ <- args$occ
 opt.ncores <- args$ncores
+opt.minCells <- args$minCells
 #opt.measurement <- args$measurement
 
 dir.create(dirname(out.prefix),F,T)
@@ -40,7 +42,7 @@ if(file.exists(sce.pb.file)){
 }else{
     tic("convertLimmaToSCE")
     sce.pb <- convertLimmaToSCE(de.limma.tb,out.prefix,ncores=8,
-                        min.ncells=30,min.ncellsStudy=200,
+                        min.ncells=opt.minCells,min.ncellsStudy=200,
                         gset.list=NULL,
                         de.mode="multiAsTwo",column.exp="meanScale",
                         TH.gene.occ=opt.gene.occ,
